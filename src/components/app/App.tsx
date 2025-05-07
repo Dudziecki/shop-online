3b8f06e86eafd9e91cc74eccbbfdcdf3f8662b39
Michael Dudzik <mkdudik15@gmail.com>
2025-05-07 19:44:52 +0300

    create app function and style

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,21 @@
+import { AppRoutes } from "../routes/routes.tsx"
+import './App.css'
+import { Header } from "../Header/Header.tsx"
+import { Sidebar } from "../Sidebar/Sidebar.tsx"
+import { Footer } from "../Footer/Footer.tsx"
 
 export const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="App">
+
+      <Header/>
+      <div className="container">
+        <Sidebar/>
+        <AppRoutes/>
+      </div>
+
+      <Footer/>
+    </div>
   )
 
 }
