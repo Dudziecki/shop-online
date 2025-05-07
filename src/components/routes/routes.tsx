c237d0b4f5b904fd3e07628e2bdb511c740d3536
Michael Dudzik <mkdudik15@gmail.com>
2025-05-07 19:45:44 +0300

    add routes

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom"
+import { ROUTES } from "../../utils/Routes.ts"
+import { Home } from "../Home/Home.tsx"
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route>
-
-      </Route>
+      <Route index path={ROUTES.HOME} element={<Home/>}/>
     </Routes>
   )
 }
