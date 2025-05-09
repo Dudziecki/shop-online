fd89e71768f5a4495b0e71a674396fbc052cbead
Michael Dudzik <mkdudik15@gmail.com>
2025-05-09 11:39:03 +0300

    add styles and  a new jsx structure

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,25 @@
+import { NavLink } from "react-router-dom";
+import s from "./Sidebar.module.css";
+
 export const Sidebar = () => {
   return (
-    <div>
-      Sidebar
-    </div>
-  )
+    <section className={s.sidebar}>
+      <div className={s.title}>CATEGORIES</div>
+      <nav>
+        <ul className={s.menu}>
+          <NavLink to="/computers" className={s.link}>Computers</NavLink>
+          <NavLink to="/clothes" className={s.link}>Clothes</NavLink>
+          <NavLink to="/shoes" className={s.link}>Shoes</NavLink>
+          <NavLink to="/furniture" className={s.link}>Furniture</NavLink>
+          <NavLink to="/cosmetics" className={s.link}>Cosmetics</NavLink>
+          <NavLink to="/travel" className={s.link}>Travel</NavLink>
+          <NavLink to="/automotive" className={s.link}>Automotive</NavLink>
+        </ul>
+      </nav>
+      <div className={s.footer}>
+        <a href="#" target="_blank" className={s.help}>Help</a>
+        <a href="#" target="_blank" className={s.terms}>Terms & Conditions</a>
+      </div>
+    </section>
+  );
 }
-
