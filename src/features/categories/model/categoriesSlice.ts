6d71be5cc9b0f23e1223a3cce9d29be6da7d8ada
Michael Dudzik <mkdudik15@gmail.com>
2025-05-10 22:55:52 +0300

    create categoriesSlice

---
+++ src/features/categories/model/categoriesSlice.ts
@@ -0,0 +1,37 @@
+import { createAppSlice } from "@/common/hooks/createAppSlice.ts"
+import { categoriesApi } from "@/features/categories/api/categoriesApi.ts"
+import type { Category } from "@/components/Products/types.ts"
+
+
+export const categoriesSlice = createAppSlice({
+  name: "categories",
+  initialState: {
+    list: [] as Category[],
+    isLoading: false ,
+  },
+  reducers: create => ({
+    getCategoriesTC: create.asyncThunk(
+      async (_, { rejectWithValue }) => {
+        try {
+          const res = await categoriesApi.getCategories()
+          return { categories: res.data  }
+        } catch (err) {
+          return rejectWithValue(null)
+        }
+      },
+      {
+        pending:(state, _) => {
+          state.isLoading=true
+        },
+        fulfilled: (state, action) => {
+          state.list = action.payload.categories
+          state.isLoading=false
+        },
+
+      },
+
+    )
+  })
+})
+export const { getCategoriesTC } = categoriesSlice.actions
+export const categoriesReducer = categoriesSlice.reducer
