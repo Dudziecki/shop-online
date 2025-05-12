316c6f882e972a73249090f75595b5225174f6ca
Michael Dudzik <mkdudik15@gmail.com>
2025-05-12 21:02:51 +0300

    add dispatch to call filterByPrice

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,33 @@
-import { Poster } from "@/components/Poster/Poster.tsx";
-import { Products } from "@/components/Products/Products.tsx";
-import { useAppSelector } from "@/common/hooks/useAppSelector.ts";
-import { selectProducts } from "@/features/products/model/productsSlice.ts";
-import s from "./Home.module.css";
+import { Poster } from "@/components/Poster/Poster.tsx"
+import { Products } from "@/components/Products/Products.tsx"
+import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
+import { filterByPrice, selectFilteredProducts, selectProducts } from "@/features/products/model/productsSlice.ts"
+import s from "./Home.module.css"
 import { Categories } from "@/components/Categories/Categories.tsx"
 import { selectCategory } from "@/features/categories/model/categoriesSlice.ts"
 import { Banner } from "@/components/Banner/Banner.tsx"
+import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import { useEffect } from "react"
 
 export const Home = () => {
-  const products = useAppSelector(selectProducts);
-  const categories = useAppSelector(selectCategory);
+  const dispatch=useAppDispatch()
+  const products = useAppSelector(selectProducts)
+  const filteredProducts = useAppSelector(selectFilteredProducts)
+  const categories = useAppSelector(selectCategory)
+  useEffect(() => {
+    if(!products.length)return
+    dispatch(filterByPrice(50))
+  },[dispatch,products])
   debugger
   return (
     <div className={s.homeContainer}>
       {/*<Poster />*/}
       {/*<div className={s.productsContainer}>*/}
-        <Products products={products} amount={5} title="Trending" />
-        <Categories categories={categories} amount={5} title="Worth seeing"/>
-      <Banner/>
+      <Products products={products} amount={5} title="Trending" />
+      <Categories categories={categories} amount={5} title="Worth seeing" />
+      <Banner />
+      <Products products={filteredProducts} amount={5} title="Less than 50$" />
       {/*</div>*/}
     </div>
-  );
-};
+  )
+}
