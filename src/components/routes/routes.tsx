b74bf973ee30c7eb38f01d288e09f14419d9360b
Michael Dudzik <mkdudik15@gmail.com>
2025-05-13 21:52:06 +0300

    add route to product

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom"
-import { ROUTES } from "../../utils/Routes.ts"
+import { ROUTES } from "@/common/utils/Routes.ts"
 import { Home } from "../Home/Home.tsx"
+import { App } from "@/components/app/App.tsx"
+import { SingleProduct } from "@/components/Products/SingleProduct.tsx"
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index path={ROUTES.HOME} element={<Home/>}/>
+      <Route  path={ROUTES.PRODUCT} element={<SingleProduct/>}/>
     </Routes>
   )
 }
