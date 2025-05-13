0ac7dd08869efe6696b784848034af0c4aadf959
Michael Dudzik <mkdudik15@gmail.com>
2025-05-13 19:27:31 +0300

    add middleware to store

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { categoriesReducer, categoriesSlice } from "@/features/categories/model/categoriesSlice.ts"
 import { productsReducer, productsSlice } from "@/features/products/model/productsSlice.ts"
+import { productApi } from "@/features/products/product/productApi.ts"
 
 export const store = configureStore({
   reducer: {
     [categoriesSlice.name]: categoriesReducer,
     [productsSlice.name]: productsReducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(productApi.middleware),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
