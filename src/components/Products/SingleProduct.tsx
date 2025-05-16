2a61d21428fa4d338d34c0da43c6c46913c1b7ff
Michael Dudzik <mkdudik15@gmail.com>
2025-05-16 23:01:01 +0300

    add Related Products,call dispatch and selector function

--- src/components/Products/SingleProduct.tsx
+++ src/components/Products/SingleProduct.tsx
@@ -3,21 +3,34 @@ import { useGetProductQuery } from "@/features/products/product/productApi.ts"
 import { useEffect } from "react"
 import { ROUTES } from "@/common/utils/Routes.ts"
 import { Product } from "@/components/Products/Product/Product.tsx"
+import { Products } from "@/components/Products/Products.tsx"
+import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
+import { getRelatedProducts, selectRelatedProducts } from "@/features/products/model/productsSlice.ts"
+import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
 
 
 export const SingleProduct = () => {
   const { id } = useParams()
+  const dispatch=useAppDispatch()
   const navigate = useNavigate()
   const {data,isFetching,isLoading,isSuccess}=useGetProductQuery({id})
+  const relatedProducts =useAppSelector(selectRelatedProducts)
   // console.log(data)
   useEffect(() => {
     if(!isFetching && !isLoading && !isSuccess){
       navigate(ROUTES.HOME)
     }
   }, [isFetching,isLoading,isSuccess])
+  useEffect(() => {
+    if(!data||relatedProducts.length)return
+
+      dispatch(getRelatedProducts(data.category.id))
+
+  }, [data,dispatch,relatedProducts.length])
   return ! data?(<section>Loading</section>):(
     <div>
-      <Product images={data?.images} title={data.title} price={data.price} description={data.description} />
+      <Product data={data} images={data?.images} title={data.title} price={data.price} description={data.description} />
+      <Products products={relatedProducts} amount={5} title="Related Products" />
     </div>
   )
 }
