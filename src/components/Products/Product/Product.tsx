a5d789ed9f364ab37c48b820089e25c50fc1ed21
Michael Dudzik <mkdudik15@gmail.com>
2025-05-16 23:05:23 +0300

    Implement product details page with cart functionality

--- src/components/Products/Product/Product.tsx
+++ src/components/Products/Product/Product.tsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom"
 import { ROUTES } from "@/common/utils/Routes.ts"
 import styles from "./Product.module.css"
+import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import { useEffect, useState } from "react"
+import { addItemToCart } from "@/features/user/userSlice.ts"
+import type { Product as ProductType } from "@/features/products/api/productsApi.types.ts"
 
 type ProductProps = {
+  data:ProductType
+
   images?: string[]
   title: string
   price: number
   description: string
 }
 const SIZES = [4.5, 5, 5.5]
-export const Product: React.FC<ProductProps> = ({ images, title, price, description }) => {
-  const currentImage = images && images.length > 0 ? images[0] : ""
+export const Product: React.FC<ProductProps> = ({ images, title, price, description ,data}) => {
+  const dispatch = useAppDispatch()
+  // const currentImage = images && images.length > 0 ? images[0] : ""
+  const [currentImage, setCurrentImage] = useState<string | undefined>(undefined)
+  const [currentSize, setCurrentSize] = useState<number | undefined>(undefined)
+  const addToCart = () => {
+    dispatch(addItemToCart({
+      product: data,
+    }))
+  }
+  useEffect(() => {
+    if (!images?.length) return
+
+    setCurrentImage(images[0])
+  }, [images])
   return (
     <section className={styles.Product}>
       <div className={styles.images}>
@@ -21,7 +40,7 @@ export const Product: React.FC<ProductProps> = ({ images, title, price, descript
               key={i}
               className={styles.image}
               style={{ backgroundImage: `url(${image})` }}
-              onClick={() => {}}
+              onClick={() => setCurrentImage(image)}
             />
           ))}
         </div>
@@ -38,8 +57,10 @@ export const Product: React.FC<ProductProps> = ({ images, title, price, descript
           <div className={styles.list}>
             {SIZES.map((size) => (
               <div
-                onClick={() => {}}
-                className={`${styles.size}`}
+                onClick={() => setCurrentSize(size)}
+                className={`${styles.size} ${
+                  currentSize === size ? "active" : ""
+                }`}
                 key={size}
               >
                 {size}
@@ -48,12 +69,12 @@ export const Product: React.FC<ProductProps> = ({ images, title, price, descript
           </div>
           <p className={styles.description}>{description}</p>
           <div className={styles.actions}>
-            <button className={styles.cart}>Add to cart</button>
+            <button className={styles.cart} onClick={addToCart}>Add to cart</button>
             <button className={styles.favorites}>Add to favorites</button>
           </div>
           <div className={styles.bottom}>
             <div className={styles.purchase}>19 purchased</div>
-            <Link to={ROUTES.HOME} />
+            <Link to={ROUTES.HOME}>Return to store</Link>
           </div>
         </div>
       </div>
