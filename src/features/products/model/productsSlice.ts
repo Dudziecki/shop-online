019ec879058127600080769d75fad86f20ae003d
Michael Dudzik <mkdudik15@gmail.com>
2025-05-16 23:02:34 +0300

    create getRelatedProducts reducer and add selectRelatedProducts selector

--- src/features/products/model/productsSlice.ts
+++ src/features/products/model/productsSlice.ts
@@ -1,6 +1,7 @@
 import { createAppSlice } from "@/common/hooks/createAppSlice.ts"
 import type { Product } from "@/components/Products/types.ts"
 import { productsApi } from "@/features/products/api/productsApi.ts"
+import { shuffle } from "@/common/utils/shuffle.ts"
 
 
 export const productsSlice = createAppSlice({
@@ -9,11 +10,12 @@ export const productsSlice = createAppSlice({
     list: [] as Product[],
     isLoading: false ,
     filtered:[] as Product[],
-    related:[]
+    related:[]as Product[]
   },
   selectors:{
     selectProducts:state=>state.list,
-    selectFilteredProducts:state=>state.filtered
+    selectFilteredProducts:state=>state.filtered,
+    selectRelatedProducts:state=>state.related
   },
   reducers: create => ({
     getProductsTC: create.asyncThunk(
@@ -33,7 +35,7 @@ export const productsSlice = createAppSlice({
         fulfilled: (state, action) => {
           state.list = action.payload.categories
           state.isLoading=false
-          debugger
+
         },
 
       },
@@ -41,9 +43,13 @@ export const productsSlice = createAppSlice({
     ),
     filterByPrice:create.reducer<number>((state, action)=>{
       state.filtered= state.list.filter(product=>product.price < action.payload)
+    }),
+    getRelatedProducts:create.reducer<number>((state, action)=>{
+      const list= state.list.filter(product=>product.category.id === action.payload)
+      state.related=shuffle(list)
     })
   })
 })
-export const { getProductsTC,filterByPrice} = productsSlice.actions
-export const {selectProducts,selectFilteredProducts}=productsSlice.selectors
+export const { getProductsTC,filterByPrice,getRelatedProducts} = productsSlice.actions
+export const {selectProducts,selectFilteredProducts,selectRelatedProducts}=productsSlice.selectors
 export const productsReducer = productsSlice.reducer
