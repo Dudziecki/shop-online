d780557b4ad91d9271154520a39ded6fa9940b98
Michael Dudzik <mkdudik15@gmail.com>
2025-05-17 23:13:49 +0300

    add new methods loginUser and etc

--- src/features/user/api/userApi.ts
+++ src/features/user/api/userApi.ts
@@ -1,8 +1,26 @@
 import { instance } from "@/common/instance/instance.ts"
-import type { UserBody, UserResponse } from "@/features/user/api/userApi.types.ts"
+import type { LoginResponse, UserBody, UserLogin, UserResponse } from "@/features/user/api/userApi.types.ts"
 
 export const userApi={
   registerUser (body:UserBody){
-    return instance.post<UserResponse>("/users",{body})
+    return instance.post<UserResponse>("/users",body)
+  },
+  loginUser(body:UserLogin){
+    return instance.post<LoginResponse>("/auth/login",body)
+  },
+  getUser(accessToken: string) {
+    return instance.get("auth/profile", {
+      headers: {
+        Authorization: `Bearer ${accessToken}` // Исправленный формат
+      }
+    })
+  },
+  getCurrentUser() {
+    const token = localStorage.getItem('token');
+    return instance.get("auth/profile", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 }
