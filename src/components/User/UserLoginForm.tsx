baef39a5258117bad730b59bef3a58d6dff230f3
Michael Dudzik <mkdudik15@gmail.com>
2025-05-18 13:36:13 +0300

    import  module styles for form

--- src/components/User/UserLoginForm.tsx
+++ src/components/User/UserLoginForm.tsx
@@ -1,21 +1,25 @@
 import { type FC, type FormEvent, useState } from "react"
 import { loginUserTC } from "@/features/user/userSlice.ts"
 import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import s from "./UserStyles.module.css" // Importing the same CSS module
 
 type UserSignUpFormProps = {
   closeForm: () => void
-  toggleFormHandler:(type:string)=>void
+  toggleFormHandler: (type: string) => void
+  isShow:boolean
 }
 
-export const UserLoginForm: FC<UserSignUpFormProps> = ({ closeForm,toggleFormHandler }) => {
+export const UserLoginForm: FC<UserSignUpFormProps> = ({ closeForm, toggleFormHandler,isShow }) => {
   const dispatch = useAppDispatch()
-  const [values, setValues] = useState<{email:string,password:string}>({
+  const [values, setValues] = useState<{email: string, password: string}>({
     email: "",
     password: ""
   })
+
   const handleChange = ({ target: { value, name } }: { target: { value: string, name: string } }) => {
     setValues({ ...values, [name]: value })
   }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const isEmpty = Object.values(values).some(val => !val)
@@ -23,47 +27,51 @@ export const UserLoginForm: FC<UserSignUpFormProps> = ({ closeForm,toggleFormHan
     dispatch(loginUserTC(values))
     closeForm()
   }
+
   return (
-    <div className={"wrapper"}>
-      <div className="close" onClick={closeForm}>
-        <svg className="icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1.375 1.375L12.625 12.625" stroke="#576067" stroke-width="2" stroke-linecap="round"
-                stroke-linejoin="round" />
-          <path d="M1.375 12.625L12.625 1.375" stroke="#576067" stroke-width="2" stroke-linecap="round"
-                stroke-linejoin="round" />
+    <div className={`${s.wrapper} ${isShow ? s.wrapperVisible : ''}`}>
+      <div className={s.close} onClick={closeForm}>
+        <svg className={s.icon} width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1.375 1.375L12.625 12.625" stroke="#576067" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M1.375 12.625L12.625 1.375" stroke="#576067" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </div>
 
-      <div className="title">Sign In</div>
-      <form className={"form"} onSubmit={handleSubmit}>
-        <div className="group">
-          <input type="email"
-                 placeholder="Your Email"
-                 name="email"
-                 value={values.email}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+      <div className={s.title}>Sign In</div>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <div className={s.group}>
+          <input
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={values.email}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
 
-
-        <div className="group">
-          <input type="password"
-                 placeholder="Your password"
-                 name="password"
-                 value={values.password}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+        <div className={s.group}>
+          <input
+            type="password"
+            placeholder="Your password"
+            name="password"
+            value={values.password}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
 
-
-        <div className="link" onClick={()=>toggleFormHandler("signup")}>Create an account</div>
-        <button type="submit" className="submit">Login</button>
+        <div className={s.link} onClick={() => toggleFormHandler("signup")}>
+          Create an account
+        </div>
+        <button type="submit" className={s.submit}>
+          Login
+        </button>
       </form>
     </div>
   )
 }
-
--- src/components/User/UserSignUpForm.tsx
+++ src/components/User/UserSignUpForm.tsx
@@ -2,90 +2,105 @@ import { type FC, type FormEvent, useState } from "react"
 import type { UserBody } from "@/features/user/api/userApi.types.ts"
 import { registerUserTC } from "@/features/user/userSlice.ts"
 import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import s from "./UserStyles.module.css" // Changed to module CSS
 
 type UserSignUpFormProps = {
   closeForm: () => void
-  toggleFormHandler:(type:string)=>void
+  toggleFormHandler: (type: string) => void
+  isShow:boolean
 }
 
-export const UserSignUpForm: FC<UserSignUpFormProps> = ({ closeForm,toggleFormHandler }) => {
-  const dispatch=useAppDispatch()
+export const UserSignUpForm: FC<UserSignUpFormProps> = ({ closeForm, toggleFormHandler,isShow }) => {
+  const dispatch = useAppDispatch()
   const [values, setValues] = useState<UserBody>({
     email: "",
     avatar: "",
     name: "",
     password: ""
   })
+
   const handleChange = ({ target: { value, name } }: { target: { value: string, name: string } }) => {
     setValues({ ...values, [name]: value })
   }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const isEmpty = Object.values(values).some(val => !val)
-    if(isEmpty)return
+    if (isEmpty) return
     dispatch(registerUserTC(values))
     closeForm()
   }
+
   return (
-    <div className={"wrapper"}>
-      <div className="close" onClick={closeForm}>
-        <svg className="icon" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1.375 1.375L12.625 12.625" stroke="#576067" stroke-width="2" stroke-linecap="round"
-                stroke-linejoin="round" />
-          <path d="M1.375 12.625L12.625 1.375" stroke="#576067" stroke-width="2" stroke-linecap="round"
-                stroke-linejoin="round" />
+    <div className={`${s.wrapper} ${isShow ? s.wrapperVisible : ''}`}>
+      <div className={s.close} onClick={closeForm}>
+        <svg className={s.icon} width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1.375 1.375L12.625 12.625" stroke="#576067" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M1.375 12.625L12.625 1.375" stroke="#576067" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </div>
 
-      <div className="title">Sign Up</div>
-      <form className={"form"} onSubmit={handleSubmit}>
-        <div className="group">
-          <input type="email"
-                 placeholder="Your Email"
-                 name="email"
-                 value={values.email}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+      <div className={s.title}>Sign Up</div>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <div className={s.group}>
+          <input
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={values.email}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
 
-        <div className="group">
-          <input type="name"
-                 placeholder="Your name"
-                 name="name"
-                 value={values.name}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+        <div className={s.group}>
+          <input
+            type="text"
+            placeholder="Your name"
+            name="name"
+            value={values.name}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
 
-        <div className="group">
-          <input type="password"
-                 placeholder="Your password"
-                 name="password"
-                 value={values.password}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+        <div className={s.group}>
+          <input
+            type="password"
+            placeholder="Your password"
+            name="password"
+            value={values.password}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
 
-        <div className="group">
-          <input type="text"
-                 placeholder="Your avatar"
-                 name="avatar"
-                 value={values.avatar}
-                 autoComplete="off"
-                 onChange={handleChange}
-                 required
+        <div className={s.group}>
+          <input
+            type="text"
+            placeholder="Your avatar"
+            name="avatar"
+            value={values.avatar}
+            autoComplete="off"
+            onChange={handleChange}
+            required
+            className={s.input}
           />
         </div>
-        <div className="link" onClick={()=>toggleFormHandler("login")}>I already have an account</div>
-        <button type="submit" className="submit" >Create an account</button>
+
+        <div className={s.link} onClick={() => toggleFormHandler("login")}>
+          I already have an account
+        </div>
+        <button type="submit" className={s.submit}>
+          Create an account
+        </button>
       </form>
     </div>
   )
 }
-
