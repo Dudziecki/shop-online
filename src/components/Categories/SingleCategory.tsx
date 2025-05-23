4c2c9206e37d9d721c26c392eb277127ee99deb1
Michael Dudzik <mkdudik15@gmail.com>
2025-05-23 17:07:18 +0300

    add poster and category

---
+++ src/components/Categories/SingleCategory.tsx
@@ -0,0 +1,13 @@
+import { Poster } from "@/components/Poster/Poster.tsx"
+import { Category } from "@/components/Categories/Category.tsx"
+
+
+export const SingleCategory = () => {
+  return (
+    <>
+      <Poster />
+      <Category/>
+    </>
+  )
+}
+
