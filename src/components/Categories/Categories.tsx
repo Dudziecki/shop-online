715a205d1f35f96ad6102c2a889b0ea596a4bcdd
Michael Dudzik <mkdudik15@gmail.com>
2025-05-29 23:52:27 +0300

    fix route problem

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import s from "@/components/Categories/Categories.module.css"
 import { Link } from "react-router-dom"
 import type { Category } from "@/features/categories/api/categoriesApi.types.ts"
+import styles from "./Categories.module.css"
 
 export interface CategoriesProps {
   title?: string;
@@ -9,22 +9,37 @@ export interface CategoriesProps {
   amount?: number;
 }
 
-export const Categories: React.FunctionComponent<CategoriesProps> = ({ categories, title, amount }) => {
+export const Categories: React.FunctionComponent<CategoriesProps> = ({
+                                                                       categories,
+                                                                       title,
+                                                                       amount
+                                                                     }) => {
   const limit = amount ?? categories.length
-
   const list = categories.filter((_, i) => i < limit)
+
   return (
-    <section className={s.section}>
-      <h2 className={s.title}>{title}</h2>
-      <div className={s.list}>
+    <section className={styles.section}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.list}>
         {list?.map((category: Category) => (
-          <Link key={category.id} to={`/categories/${category.id}`} className={s.item}>
-            <div className={s.image} style={{ backgroundImage: `url(${category.image})` }} />
-            <h3 className={s.name}>{category.name}</h3>
+          <Link
+            key={category.id}
+            to={`/categories/${category.id}`}
+            className={styles.item}
+          >
+            <div
+              className={styles.imageWrapper}
+            >
+              <div
+                className={styles.image}
+                style={{ backgroundImage: `url(${category.image})` }}
+              />
+              <div className={styles.overlay}></div>
+            </div>
+            <h3 className={styles.name}>{category.name}</h3>
           </Link>
         ))}
       </div>
     </section>
   )
 }
-
