3b309f81293205173aca3ba009621de68e8e3b4c
Michael Dudzik <mkdudik15@gmail.com>
2025-05-30 22:56:08 +0300

    remove old product type

--- src/features/products/api/productsApi.types.ts
+++ src/features/products/api/productsApi.types.ts
@@ -5,12 +5,3 @@ export interface Category {
   slug: string;
 }
 
-export interface Product {
-  id: number;
-  title: string;
-  slug: string;
-  price: number;
-  description: string;
-  category: Category;
-  images: string[];
-}
