d9d932aa0653edfdf9bb179a9f271677fdfb65d8
Michael Dudzik <mkdudik15@gmail.com>
2025-05-30 22:56:30 +0300

    add new routes

---
+++ src/common/utils/Routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  HOME:'/',
+  CART:'/cart',
+  PROFILE:'/profile',
+  CATEGORIES:'/categories/:id',
+  PRODUCT:'/products/:id',
+  FAVOURITES:'/favorites',
+  HELP:'/help',
+  TERMS:'/terms',
+  MAINPRODUCT:'/mainproduct',
+}
