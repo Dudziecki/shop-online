107d6b36cc6da33fa8a5ce150c38b6d72d18030f
Michael Dudzik <mkdudik15@gmail.com>
2025-05-31 13:12:27 +0300

    instead of static data, now the data comes from the server and is displayed properly

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,58 @@
-import { NavLink } from "react-router-dom";
-import s from "./Sidebar.module.css";
+import { NavLink } from "react-router-dom"
+import { ROUTES } from "@/common/utils/Routes.ts"
+import { useAppSelector } from "@/common/hooks/useAppSelector.ts"
+import { selectCategory } from "@/features/categories/model/categoriesSlice.ts"
+import { useEffect } from "react"
+import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
+import { getCategoriesTC } from "@/features/categories/model/categoriesSlice.ts"
+import styles from "./Sidebar.module.css"
 
 export const Sidebar = () => {
+  const dispatch = useAppDispatch()
+  const categories = useAppSelector(selectCategory)
+  const isLoading = useAppSelector(state => state.categories.isLoading)
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      dispatch(getCategoriesTC())
+    }
+  }, [dispatch, categories.length])
+
+  const displayedCategories = categories.slice(0, 5);
+
   return (
-    <section className={s.sidebar}>
-      <div className={s.title}>CATEGORIES</div>
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
       <nav>
-        <ul className={s.menu}>
-          <NavLink to={`/categories/${1}`} className={s.link}>Computers</NavLink>
-          <NavLink to={`/categories/${2}`} className={s.link}>Clothes</NavLink>
-          <NavLink to={`/categories/${3}`} className={s.link}>Shoes</NavLink>
-          <NavLink to={`/categories/${4}`} className={s.link}>Furniture</NavLink>
-          <NavLink to={`/categories/${5}`} className={s.link}>Cosmetics</NavLink>
-          <NavLink to="/travel" className={s.link}>Travel</NavLink>
-          <NavLink to="/automotive" className={s.link}>Automotive</NavLink>
-        </ul>
+        {isLoading ? (
+          <ul className={styles.menu}>
+            {[...Array(5)].map((_, i) => (
+              <div key={i} className={styles.skeletonLink}></div>
+            ))}
+          </ul>
+        ) : (
+          <ul className={styles.menu}>
+            {displayedCategories.map(category => (
+              <NavLink
+                key={category.id}
+                to={`/categories/${category.id}`}
+                className={({ isActive }) =>
+                  `${styles.link} ${isActive ? styles.active : ''}`
+                }
+              >
+                {category.name}
+              </NavLink>
+            ))}
+            {categories.length > 5 && (
+              <div className={styles.more}>...</div>
+            )}
+          </ul>
+        )}
       </nav>
-      <div className={s.footer}>
-        <a href="#" target="_blank" className={s.help}>Help</a>
-        <a href="#" target="_blank" className={s.terms}>Terms & Conditions</a>
+      <div className={styles.footer}>
+        <NavLink to={ROUTES.HELP} className={styles.help}>Help</NavLink>
+        <NavLink to={ROUTES.TERMS} className={styles.help}>Terms & Conditions</NavLink>
       </div>
     </section>
-  );
+  )
 }
