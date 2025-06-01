1a249c84578f771da8317e270372a15d09f63efe
Michael Dudzik <mkdudik15@gmail.com>
2025-06-01 22:25:34 +0300

    add new methods

--- src/features/user/api/userApi.ts
+++ src/features/user/api/userApi.ts
@@ -1,26 +1,43 @@
 import { instance } from "@/common/instance/instance.ts"
-import type { LoginResponse, UserBody, UserLogin, UserResponse } from "@/features/user/api/userApi.types.ts"
+import type {
+  LoginResponse,
+  UpdateRequest,
+  UserBody,
+  UserLogin,
+  UserResponse
+} from "@/features/user/api/userApi.types.ts"
 
-export const userApi={
-  registerUser (body:UserBody){
-    return instance.post<UserResponse>("/users",body)
+export const userApi = {
+  registerUser(body: UserBody) {
+    return instance.post<UserResponse>("/users", body)
   },
-  loginUser(body:UserLogin){
-    return instance.post<LoginResponse>("/auth/login",body)
+  loginUser(body: UserLogin) {
+    return instance.post<LoginResponse>("/auth/login", body, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
   },
   getUser(accessToken: string) {
-    return instance.get("auth/profile", {
+    return instance.get<UserResponse>("auth/profile", {
       headers: {
-        Authorization: `Bearer ${accessToken}` // Исправленный формат
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+  },
+  updateUser(body: UpdateRequest) {
+    return instance.put<UserResponse>(`users/${body.id}`, body, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     })
   },
   getCurrentUser() {
-    const token = localStorage.getItem('token');
-    return instance.get("auth/profile", {
+    const token = localStorage.getItem('token')
+    return instance.get<UserResponse>("auth/profile", {
       headers: {
         Authorization: `Bearer ${token}`
       }
-    });
+    })
   }
 }
