1a490ff3c49ab6f40e46eefe901de0a6d883ca33
Michael Dudzik <mkdudik15@gmail.com>
2025-06-03 23:42:35 +0300

    // add other environment variables you use

--- globals.d.ts
+++ globals.d.ts
@@ -12,3 +12,12 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string
+  readonly VITE_API_KEY: string
+  // добавьте другие переменные окружения, которые вы используете
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
