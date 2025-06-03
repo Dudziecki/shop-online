66bab0807e67ac592d91912699fe23130cacec8b
Michael Dudzik <mkdudik15@gmail.com>
2025-06-03 23:41:42 +0300

    add Profile form and main product route path

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -8,19 +8,20 @@ import { SingleCategory } from "@/components/Categories/SingleCategory.tsx"
 import { Cart } from "@/components/Cart/Cart.tsx"
 import { Favourites } from "@/components/Favourites/Favourites.tsx"
 import { HelpPage } from "@/components/HelpPage/HelpPage.tsx"
-import { ShowNowProduct } from "@/components/ShopNowProduct/ShowNowProduct.tsx"
+import { ShopNowProduct, ShowNowProduct } from "@/components/ShopNowProduct/ShowNowProduct.tsx"
+import { ProfileForm } from "@/components/User/ProfileForm/ProfileForm.tsx"
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
-      <Route path={ROUTES.PROFILE} element={<Profile />} />
+      <Route path={ROUTES.PROFILE} element={<ProfileForm />} />
       <Route path={ROUTES.CATEGORIES} element={<SingleCategory/>} />
       <Route path={ROUTES.CART} element={<Cart/>} />
       <Route path={ROUTES.FAVOURITES} element={<Favourites/>} />
       <Route path={ROUTES.HELP} element={<HelpPage/>} />
-      <Route path={ROUTES.MAINPRODUCT} element={<ShowNowProduct/>} />
+      <Route path={ROUTES.MAINPRODUCT} element={<ShopNowProduct/>} />
     </Routes>
   )
 }
