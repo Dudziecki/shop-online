d5f40a42f92e75d050d44c0c85e81e324d22115c
Michael Dudzik <mkdudik15@gmail.com>
2025-06-04 16:52:30 +0300

    remove home import

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,11 @@
 import { AppRoutes } from "../routes/routes.tsx"
 import "./App.css"
 import { Header } from "../Header/Header.tsx"
-import { Sidebar } from "../Sidebar/Sidebar.tsx"
 import { Footer } from "../Footer/Footer.tsx"
 import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts"
 import { useEffect } from "react"
 import { getCategoriesTC } from "@/features/categories/model/categoriesSlice.ts"
 import { getProductsTC } from "@/features/products/model/productsSlice.ts"
-import { Home } from "@/components/Home/Home.tsx"
 import { UserForm } from "@/components/User/UserForm.tsx"
 import { checkAuth } from "@/features/user/userSlice.ts"
 
