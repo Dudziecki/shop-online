03c1738ded52fcf66ec708581e8211edd05ee7aa
Michael Dudzik <mkdudik15@gmail.com>
2025-06-05 23:09:23 +0300

    Change product type import and categories type

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,9 @@
 import { Poster } from "@/components/Poster/Poster.tsx";
-import { Products } from "@/components/Products/Products.tsx";
+import { Products } from "@/components/Products/Products/Products.tsx";
 import { useAppSelector } from "@/common/hooks/useAppSelector.ts";
 import { filterByPrice, selectFilteredProducts, selectProducts } from "@/features/products/model/productsSlice.ts";
 import s from "./Home.module.css";
-import { Categories } from "@/components/Categories/Categories.tsx";
+import { Categories } from "@/components/Categories/Categories/Categories.tsx";
 import { selectCategory } from "@/features/categories/model/categoriesSlice.ts";
 import { Banner } from "@/components/Banner/Banner.tsx";
 import { useAppDispatch } from "@/common/hooks/useAppDispatch.ts";
