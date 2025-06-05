bf18739693b56e3a2b4a80c20cee2bb05f8ba3a5
Michael Dudzik <mkdudik15@gmail.com>
2025-06-05 23:09:45 +0300

    remove poster component

--- src/components/Categories/SingleCategory.tsx
+++ src/components/Categories/SingleCategory.tsx
@@ -1,11 +1,9 @@
-import { Poster } from "@/components/Poster/Poster.tsx"
 import { Category } from "@/components/Categories/Category.tsx"
 
 
 export const SingleCategory = () => {
   return (
     <>
-      {/*<Poster />*/}
       <Category/>
     </>
   )
